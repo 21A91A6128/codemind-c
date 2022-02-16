e35f896531176c87481117a3b90ee64c33021645
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-02-16 17:55:28 +0530

    #include<stdio.h> int main() {     int n,r,s=0,t;     scanf("%d",&n);     t=n;     while(n>0)     {         r=n%10;         s=s+r;         n=n/10;     }     if(t%s==0)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

---
+++ Harshed_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,s=0,t;
+    scanf("%d",&n);
+    t=n;
+    while(n>0)
+    {
+        r=n%10;
+        s=s+r;
+        n=n/10;
+    }
+    if(t%s==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+}
