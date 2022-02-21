f2cd5dfb177d32215d63dfe22477f10c2ae85025
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-02-21 10:39:48 +0530

    #include<stdio.h> int main() {     int n,v,k;     scanf("%d",&n);     for(v=1;v<=n;v++)     {         for(k=1;k<=n;k++)         {             if(k==1||v==n||k==v)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
+    int n,v,k;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(v=1;v<=n;v++)
     {
-        for(j=1;j<=i;j++)
+        for(k=1;k<=n;k++)
         {
-            printf("%d",j);
+            if(k==1||v==n||k==v)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
