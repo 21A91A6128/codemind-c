65dcac5a66a6bec6b78721e69218e1e30bdd346d
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-04-26 19:52:47 +0530

    #include<stdio.h> int main() {     int n,j,m=0;     scanf("%d",&n);     for(j=0;j<=(n/2);j++)         {             if(j*j==n)             {                 m++;             }         }         if(m==1)         {             printf("True ");         }         else         {             printf("False ");         }     return 0; }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,15 +1,24 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
 int main()
 {
-int num,v;
-float k;
-scanf("%d",&num);
-k=sqrt((double)num);
-v=k;
-if(v==k)
-printf("True");
-else
-printf("False");
-return 0;
+    int n,j,m=0;
+    scanf("%d",&n);
+    for(j=0;j<=(n/2);j++)
+        {
+            if(j*j==n)
+            {
+                m++;
+            }
+        }
+        if(m==1)
+        {
+            printf("True
+");
+        }
+        else
+        {
+            printf("False
+");
+        }
+    return 0;
 }
