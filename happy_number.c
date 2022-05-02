c0d18e240f1e303b273c4b2dd84948c7b7d422dc
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-05-02 11:35:12 +0530

    #include<stdio.h> int main() {     int n,r,s=0;     scanf("%d",&n);     while(n)     {         r=n%10;         s+=r*r;         n/=10;         if(n==0 && s>9)         {             n=s;             s=0;         }     }     if(s!=1)     {         printf("False");     }     else     {         printf("True");     } }

--- happy_number.c
+++ happy_number.c
@@ -1,26 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,s=0,t;
+    int n,r,s=0;
     scanf("%d",&n);
-    while(n>0)
+    while(n)
     {
         r=n%10;
         s+=r*r;
-        n=n/10;
-        if(n==0 && s>=10)
+        n/=10;
+        if(n==0 && s>9)
         {
             n=s;
             s=0;
         }
     }
-    if(s==1)
-    {
-        printf("True");
-    }
-    else
+    if(s!=1)
     {
         printf("False");
     }
-    return 0;
+    else
+    {
+        printf("True");
+    }
 }
